e828c985c3454a62e91e0fa59a8bfffad9b4cd5a
ginopc <ginopc@tiscali.it>
2017-03-22 18:45:53 +0100

    add OpenWeatherInfo appID and review scripts

--- MyLocalWeather/js/scripts.js
+++ MyLocalWeather/js/scripts.js
@@ -5,94 +5,63 @@
 	Created : 2016.08.31
  */
 
- 
+/* Main AngularJS application declaration */
 var app = angular.module('weather', []);
 
+/* WeatherAPI Factory */
 app.factory('WeatherApi', function($http) {
-  var obj = {};
+  var wapi = {};
   
-  obj.getLoc = function() {
-    return $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK");
+  /* retrive location from client IP address */
+  wapi.getLocation = function() {
+    return $http.get("http://ipinfo.io/json?callback=JSON_CALLBACK");
   };
 	
-  obj.getCurrent = function(city) {
+  /* return weather infos for city */
+  wapi.getWeatherInfo = function(city) {
+	var owmID = '88345ddd8559d9793da76c8dfb3ed7e0';
     var api = "http://api.openweathermap.org/data/2.5/weather?q=";
     var units = "&units=metric";
-    var appid = "&APPID=061f24cf3cde2f60644a8240302983f2"
+    var appid = "&APPID=" + owmID;
     var cb = "&callback=JSON_CALLBACK";
-	  
-	  /*
-	  JSON DATA SAMPLE
-	  {
-	     "coord":{
-	  	     "lon":-0.13,
-			  "lat":51.51
-		  },
-		  "weather":[
-		     {
-			    "id":802,
-				 "main":"Clouds",
-				 "description":"scattered clouds",
-				 "icon":"03d"
-			  }
-		  ],
-		  "base":"stations",
-		  "main":{
-			  "temp":291.96,
-			  "pressure":1022,
-			  "humidity":54,
-			  "temp_min":290.93,
-			  "temp_max":293.71
-		  },
-		  "wind":{
-		     "speed":4.91,
-			  "deg":202.503
-		  },
-		  "rain":{},
-		  "clouds":{"all":48},
-		  "dt":1474901851,
-		  "sys":{
-		     "type":3,
-			  "id":258730,
-			  "message":0.0049,
-			  "country":"GB",
-			  "sunrise":1474869277,
-			  "sunset":1474912044
-		  },
-		  "id":2643743,
-		  "name":"London",
-		  "cod":200
-		}
-		*/
     
     return $http.jsonp(api + city + units+ appid + cb);
   };
 	
-  return obj
+  return wapi
 });
 
+/* Main controller */
 app.controller('WeatherController', function($scope, WeatherApi) {
   $scope.Data = {};
   $scope.Data.unit ='C';
   $scope.Data.sysChange = false;
 	
-  WeatherApi.getLoc().success(function(data) {
-	 console.log("WeatherApi.getLoc() returned some data");
-    var cityLoc = data.city + "," + data.country;
-    $scope.Data.city = data.city;
-    $scope.Data.country = data.country;
-    WeatherApi.getCurrent(cityLoc).success(function(data) {
-		console.log("WeatherApi.getCurrent() returned some data");
-    	CurrentWeather(data)
-    });
-  });
+  WeatherApi.getLocation()
+	.success(function(data){
+		console.log("WeatherApi.getLoc() returned some data");
+		var cityLoc = data.city + "," + data.country;
+		$scope.Data.city = data.city;
+		$scope.Data.country = data.country;
+		WeatherApi.getWeatherInfo(cityLoc)
+			.success(function(data) {
+				console.log("WeatherApi.getCurrent() returned some data");
+				CurrentWeather(data)
+			})
+			.error(function(){
+				alert('Error occurred retrieving weather infos');
+			});
+	})
+	.error(function(){
+		alert('Error occurred retrieving ipinfo data');
+	});
 
 	function CurrentWeather(data) {
 	  	console.log("WeatherApi.getLoc() called");
 		$scope.Data.temp = Math.round(data.main.temp);
-		$scope.Data.Cel = Math.round(data.main.temp);
+		$scope.Data.TempC = Math.round(data.main.temp);
 		$scope.Data.des = data.weather[0].main;
-		$scope.Data.Fah = Math.round( ($scope.Data.temp * 9)/5 + 32 );
+		$scope.Data.TempF = Math.round( ($scope.Data.temp * 9)/5 + 32 );
 		return IconGen($scope.Data.des);
 	}
 
@@ -135,14 +104,14 @@ app.controller('WeatherController', function($scope, WeatherApi) {
 	  console.log('Data Unit: ' + $scope.Data.unit);
 	  switch($scope.Data.unit){
 		  case 'F': // Farenheit => Celsius
-			  $scope.Data.temp = $scope.Data.Cel;
+			  $scope.Data.temp = $scope.Data.TempC;
 			  $scope.Data.unit = 'C';
 			  result = false;
 			  break;
 			  
 		  default: // Celsius (default) => Farenheit
 			  $scope.Data.unit = 'F';
-			  $scope.Data.temp = $scope.Data.Fah;
+			  $scope.Data.temp = $scope.Data.TempF;
 			  result = true;
 			  break;
 	  }
