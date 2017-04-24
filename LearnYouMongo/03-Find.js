3a6b4a4b530f087daa99763976d2404348b00b8b
ginopc <ginopc@tiscali.it>
2017-04-24 14:37:01 +0000

    LearnYouMongo - Step 3 passed

---
+++ LearnYouMongo/03-Find.js
@@ -0,0 +1,36 @@
+/* ========================================
+ *   LearnYouMongo - Find
+ *
+ *   Author  : Maurizio Aru
+ *   Created : 2017.04.24
+ * ========================================
+ */
+ 
+var mongo = require('mongodb').MongoClient
+var dbUrl = "mongodb://localhost:27017/learnyoumongo"
+
+// check command line arguments
+if (process.argv.count < 3) {
+   
+   console.error("[ERROR]: parameters missing");
+}
+
+var age = process.argv[2];
+
+// open database connection
+mongo.connect(dbUrl, function(err, db){
+   
+    if (err) throw err;
+
+    var parrots = db.collection('parrots');
+    parrots.find({
+       age: {
+          $gt: +age
+       }
+    }).toArray(function(err, docs){
+      if (err) throw err;
+      
+      console.log(docs)
+      db.close();
+    });
+ });
